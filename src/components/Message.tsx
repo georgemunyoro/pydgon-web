b786b8beeeaf04b96e90d383345c5598559da11f
George Munyoro <george.guva@outlook.com>
2020-11-17 13:38:22 +0200

    Implement mercury_web-17: Implement link preview embed feature

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -3,6 +3,9 @@ import VizSensor from "react-visibility-sensor";
 
 import { Pane, Text, defaultTheme } from "evergreen-ui";
 
+// @ts-ignore
+import { ReactTinyLink } from "react-tiny-link";
+
 import { useSelector } from "react-redux";
 import { RootState } from "../reducers";
 
@@ -25,6 +28,8 @@ const Message: React.FC<Props> = ({ message, loggedInUser, socket }) => {
     (state: RootState) => state.currentChatUser.uuid
   );
   const [read, setRead] = useState(message.read);
+  const [containsLink, setContainsLink] = useState(false);
+  const [link, setLink] = useState("");
 
   function initSocketListener() {
     if (message.id != null) {
@@ -34,8 +39,26 @@ const Message: React.FC<Props> = ({ message, loggedInUser, socket }) => {
     }
   }
 
+  function extractUrls(string: String): string[] | undefined {
+    const matches = string.match(
+      "https?://([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?"
+    );
+    return matches?.filter((match: any) => match != null);
+  }
+
+  function checkIfMessageContainsLink() {
+    if (message.content != null) {
+      const links = extractUrls(message.content);
+      if (links !== undefined) {
+        setContainsLink(true);
+        setLink(links[0]);
+      }
+    }
+  }
+
   useEffect(() => {
     initSocketListener();
+    checkIfMessageContainsLink();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [message, chat_uuid]);
 
@@ -61,21 +84,31 @@ const Message: React.FC<Props> = ({ message, loggedInUser, socket }) => {
         marginX={15}
         display="flex"
         borderRadius={3}
+        flexDirection="column"
         maxWidth="70%"
         wordWrap="break-word"
-        wordBreak="break-all"
+        wordBreak="break-word"
         whiteSpace="pre-wrap"
-        background={
-          message.sender === loggedInUser
-            ? defaultTheme.colors.background.greenTint
-            : defaultTheme.colors.background.blueTint
+        background={message.sender === loggedInUser
+          ? defaultTheme.colors.background.greenTint
+          : defaultTheme.colors.background.blueTint
         }
         hoverElevation={2}
         style={{
-          width: "max-content",
+          width: containsLink ? "50%" : "max-content",
           marginLeft: message.sender === loggedInUser ? "auto" : "none",
         }}
       >
+        {containsLink && (
+          <ReactTinyLink
+            cardSize="small"
+            showGraphic={true}
+            maxLine={2}
+            minLine={1}
+            width="100%"
+            url={link}
+          />
+        )}
         <Text margin={10}>{message.content}</Text>
       </Pane>
     </VizSensor>
