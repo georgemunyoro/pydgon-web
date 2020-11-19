28e038ad5e03a6e0663cb866bef1d73b82eb459d
George Munyoro <george.guva@outlook.com>
2020-11-19 15:16:42 +0200

    Fix incorrect import

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from '../App';
+import React from "react";
+import { shallow } from "enzyme";
+import { App } from "../App";
 
-describe('App', () => {
-  it('Renders application', () => {
+describe("App", () => {
+  it("Renders application", () => {
     const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
   });
